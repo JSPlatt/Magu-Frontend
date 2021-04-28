66070cd9a5bdacce0086ef51806877c6ac8197ca
Jeffrey Platt <JSPlatt@gmail.com>
2021-04-28 17:34:02 -0400

    ...

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Store = (props) => {
     const[store, setStore] = useState([])
     const [review, setReview] = useState ([])
     
-
     useEffect(() => {
-        console.log(props)
+
+            const slug = props.match.params.slug
+            const url = `http://localhost:3001/stores/${slug}`
+        fetch(url)
+            .then(res => console.log(res))
+            .then (res => console.log(res))
+
+        
     }, [])
+
+    //     axios.get(url)
+    //         .then( resp => console.log(resp) )
+    //         .catch( resp => console.log(resp) )
+    // }, [])
+// NEED TO GET THIS TO WORK!!!!!!!!
     return (
         <div>
             This is the singular store index
