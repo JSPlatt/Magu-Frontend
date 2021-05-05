905676e2b4d26d06b5df4f79c898d23d3756f4b4
Jeffrey Platt <JSPlatt@gmail.com>
2021-05-05 11:22:55 -0400

    menu designed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,90 @@
 import React from 'react'
+import styled from 'styled-components'
+import ProductCard from './ProductCard'
+import { useHistory } from 'react-router-dom'
+
+
+const Home = styled.div `
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1200px;
+`
+const Header = styled.div `
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+      font-size: 42px;
+  }
+`
+const Subheader = styled.div `
+  font-weight:300;
+  font-size: 26px;
+
+  img {
+    height: 60h;
+    width: 60vh;
+    border-radius: 100%;
+    border: 5px solid #efefef;
+  }
+`
+const Grid = styled.div `
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
+const LogOutBtn = styled.button`
+color: #fff;
+background: #318231;
+border-radius: 4px;
+padding: 12px;
+font-size: 15px;
+cursor: pointer;
+transition: ease-in-out 0.1s;
+border: 1px solid #fff;
+width: 15%;
+margin-top: 0 px;
+float: left;
+justify-content: flex-start;
+
+&:hover {
+    background: #fff;
+    color: #000;
+    border: 1px solid #fff;
+}`
 
 function Menu() {
-    return (
-        <div>
-            <h1>Menu Page</h1>
-        </div>
-    )
+
+const history = useHistory ()
+function logOut()
+{
+    localStorage.clear()
+    history.push('../login')
 }
+
+    return (
+        <Home className="home">
+            <LogOutBtn onClick={logOut}>Log Out</LogOutBtn>
+            <Header>
+                <Subheader><img src={"https://www.preparedfoods.com/ext/resources/images/2019/07/Curaleaf-logo_web.jpg?1564417088"} alt="logo"></img></Subheader>
+            </Header>
+            <Grid>
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </Grid>
+        </Home>
+        )
+    }
+
 
 export default Menu
---
+++ src/components/ProductCard.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+  text-align: center;
+`
+const StoreLogo = styled.div`
+  width: 50px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 10px;
+  
+  img {
+      height: 50px;
+      width: 50px;
+      border-radius: 100%;
+      border: 1px solid #efefef;
+  }
+`
+const StoreName = styled.div`
+  padding: 20px 0 10px 0;
+`
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height: 50px;
+
+  a {
+      color: #fff;
+      background: #0D6C0D;
+      border-radius: 4px;
+      padding: 10px 50px;
+      border: 1px solid #000;
+      width: 100%;
+      text-decoration: none;
+  }
+`
+
+function ProductCard() {
+    return (
+        <Card>
+            <StoreLogo>
+                <img src={"https://s3-us-west-1.amazonaws.com/dataowl-2/production/pictures/2308-a1f88dc93d61b76200b4663d1620195562869.jpg"} alt="logo"></img>
+            </StoreLogo>
+            <StoreName>[product description]</StoreName>
+            <div className="store-score"></div>
+        </Card>
+    )
+}
+
+export default ProductCard
